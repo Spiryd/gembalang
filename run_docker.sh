6f15033d655f9889fab5f2880063037b249949c0
Spiryd <neumann.maks@gmail.com>
2024-01-18 21:39:02 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,4 @@ docker stop gembalang
 docker rm gembalang
 docker build . -t gembalang --network host
 docker run --name gembalang -d -i -t gembalang /bin/sh
-docker exec -it gembalang sh
+docker exec -it gembalang bash
